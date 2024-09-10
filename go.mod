f4ebf3758ff904a4935836a17f827f8e941d12e0
Berkay Çubuk <berkay@berkaycubuk.com>
2024-09-10 21:50:16 +0300

    switched to raylib and gui for modern features

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/berkaycubuk/magicedit
 go 1.22.3
 
 require (
-	golang.org/x/sys v0.23.0 // indirect
+	github.com/ebitengine/purego v0.7.1 // indirect
+	github.com/gen2brain/raylib-go/raylib v0.0.0-20240905170917-123ec7ae6b35 // indirect
+	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/term v0.23.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,159 +1,171 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+	"strings"
 
-	"golang.org/x/term"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-func clearScreen() {
-	fmt.Print("\033[2J") // clear screen
-	fmt.Print("\033[H") // move cursor to top left
-}
-
-func printInfoBar(height int, width int, mode string, row int, col int) {
-	fmt.Printf("\033[%d;1H", height) // move the cursor to the bottom left
-
-	fmt.Printf("%s	%d:%d", mode, row, col)
-}
-
-func moveCursor(row int, col int) {
-	fmt.Printf("\033[%d;%dH", row, col)
-}
-
 func main() {
-	args := os.Args[1:]
+	rl.InitWindow(800, 450, "magicedit v0.1.0")
+	defer rl.CloseWindow()
 
-	var lines []string
+	var textBuffer strings.Builder
+	/*
+	col := 0
+	row := 0
+	*/
 
-	if len(args) > 0 {
-		// load the file
-		f, err := os.Open(args[0])
-		if err != nil {
-			panic(err)
-		}
-		defer f.Close()
+	maxTextLength := 1000
 
-		scanner := bufio.NewScanner(f)
-		for scanner.Scan() {
-			lines = append(lines, scanner.Text())
-		}
+	cursorPos := 0
+	cursorLine := 0
+    cursorCol := 0
 
-		fmt.Println(len(lines))
-		return
-	}
+	isHoldingBackspace := false
+	isHoldingEnter := false
 
-	if len(lines) == 0 {
-		lines = append(lines, "")
-		lines = append(lines, "")
-	}
+	backspaceTimer := float32(0)
+	enterTimer := float32(0)
 
-	clearScreen()
+	keyRepeatDelay := float32(0.5)
+	keyRepeatRate := float32(0.05)
 
-	oldState, err := term.MakeRaw(int(os.Stderr.Fd()))
-	if err != nil {
-		panic(err)
-	}
-	defer term.Restore(int(os.Stdin.Fd()), oldState)
+	fontSize := 20
+	lineHeight := fontSize
+	mainFont := rl.LoadFontEx("fonts/jetbrainsmono-regular.ttf", int32(fontSize), nil, 250)
+	defer rl.UnloadFont(mainFont)
 
-	width, height, err := term.GetSize(int(os.Stdout.Fd()))
-	if err != nil {
-		panic(err)
-	}
+	rl.SetTargetFPS(60)
 
-	row := 1
-	col := 1
+	for !rl.WindowShouldClose() {
+		deltaTime := rl.GetFrameTime()
 
-	fmt.Println("MagicEdit v0.1.0")
+		pressedChar := rl.GetCharPressed()
 
-	mode := "NORMAL"
+		for pressedChar > 0 {
+			if textBuffer.Len() < maxTextLength {
+				textBuffer.WriteRune(rune(pressedChar))
+				cursorPos++
+				cursorLine, cursorCol = getCursorLineCol(textBuffer.String(), cursorPos)
+			}
 
-	isCleared := false
-
-	for {
-		var buf [1]byte
-		os.Stdin.Read(buf[:])
-
-		if !isCleared {
-			isCleared = true
-			clearScreen()
+			pressedChar = rl.GetCharPressed()
 		}
 
-		if mode == "NORMAL" {
-		} else if mode == "INSERT" {
-			if buf[0] == 13 {
-				row += 1
-				col = 1
-				if len(lines) <= row {
-					lines = append(lines, "")
+		if rl.IsKeyDown(rl.KeyBackspace) {
+			if !isHoldingBackspace {
+				if cursorPos > 0 {
+					textStr := textBuffer.String()
+					textStr = textStr[:cursorPos - 1] + textStr[cursorPos:]
+					textBuffer.Reset()
+					textBuffer.WriteString(textStr)
+					cursorPos--
+					cursorLine, cursorCol = getCursorLineCol(textBuffer.String(), cursorPos)
 				}
-			} else if buf[0] == 27 {
+
+				isHoldingBackspace = true
+				backspaceTimer = 0
 			} else {
-				//fmt.Printf("%c", buf[0])
-				col += 1
-				lines[row] = lines[row] + string(buf[0])
+				backspaceTimer += deltaTime
+				if backspaceTimer > keyRepeatDelay {
+					if backspaceTimer - keyRepeatDelay > keyRepeatRate {
+						if cursorPos > 0 {
+							textStr := textBuffer.String()
+							textStr = textStr[:cursorPos - 1] + textStr[cursorPos:]
+							textBuffer.Reset()
+							textBuffer.WriteString(textStr)
+							cursorPos--
+							cursorLine, cursorCol = getCursorLineCol(textBuffer.String(), cursorPos)
+						}
+						backspaceTimer = keyRepeatDelay
+					}
+				}
 			}
+		} else {
+			isHoldingBackspace = false
 		}
 
-		if mode == "NORMAL" && buf[0] == 'j' {
-			row += 1
-			if row > len(lines) - 1 {
-				row = len(lines) - 1
+		if rl.IsKeyDown(rl.KeyEnter) {
+			if !isHoldingEnter {
+				textBuffer.WriteString("\n")
+				cursorPos++
+				cursorLine, cursorCol = getCursorLineCol(textBuffer.String(), cursorPos)
+
+				isHoldingEnter = true
+				enterTimer = 0
+			} else {
+				enterTimer += deltaTime
+				if enterTimer > keyRepeatDelay {
+					if enterTimer - keyRepeatDelay > keyRepeatRate {
+						textBuffer.WriteString("\n")
+						cursorPos++
+						cursorLine, cursorCol = getCursorLineCol(textBuffer.String(), cursorPos)
+						enterTimer = keyRepeatDelay
+					}
+				}
+			}
+		} else {
+			isHoldingEnter = false
+		}
+
+		/* Draw Area */
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.Black)
+
+		textStr := textBuffer.String()
+		lines := strings.Split(textStr, "\n")
+		yOffset := float32(10) // Y offset for the text
+		
+		for i, line := range lines {
+			if i == cursorLine {
+                cursorX := rl.MeasureTextEx(mainFont, line[:cursorCol], float32(fontSize), 1).X
+                cursorY := yOffset + float32(i)*float32(lineHeight)
+				rl.DrawRectangle(int32(cursorX+10), int32(cursorY), 2, int32(lineHeight), rl.White)
 			}
 
-			if col > len(lines[row]) + 1 {
-				col = len(lines[row]) + 1
-			}
+			rl.DrawTextEx(mainFont, line, rl.NewVector2(10, yOffset+float32(i)*float32(lineHeight)), float32(fontSize), 1, rl.White)
 		}
 
-		if mode == "NORMAL" && buf[0] == 'k' {
-			row -= 1
-			if row < 1 {
-				row = 1
-			}
+		/*
+		// cursor
+		cursorPosMeasured := rl.MeasureTextEx(mainFont, textStr[:cursorPos], float32(fontSize), 1)
+		rl.DrawRectangle(int32(cursorPosMeasured.X), int32(cursorPosMeasured.Y), 2, int32(fontSize), rl.White)
 
-			if col > len(lines[row]) + 1 {
-				col = len(lines[row]) + 1
-			}
-		}
+		// text
+		rl.DrawTextEx(mainFont, textStr, rl.NewVector2(10, 10), float32(fontSize), 1, rl.White)
+		*/
 
-		if mode == "NORMAL" && buf[0] == 'l' {
-			col += 1
-			if col > len(lines[row]) {
-				col = len(lines[row])
-			}
-		}
-
-		if mode == "NORMAL" && buf[0] == 'h' {
-			col -= 1
-			if col < 1 {
-				col = 1
-			}
-		}
-
-		if mode == "NORMAL" && buf[0] == '0' {
-			col = 1
-		}
-
-		if mode == "NORMAL" && buf[0] == '$' {
-			col = len(lines[row])
-		}
-
-		if buf[0] == 'i' {
-			mode = "INSERT"
-		}
-
-		if buf[0] == 27 { // esc
-			mode = "NORMAL"
-		}
-
-		if mode == "NORMAL" && buf[0] == 'q' {
-			break
-		}
-
-		printInfoBar(height, width, mode, row, col)
-		moveCursor(row, col)
+		rl.EndDrawing()
+		/* Draw Area */
 	}
+}
+
+func getCursorLineCol(text string, pos int) (line, col int) {
+    lines := strings.Split(text[:pos], "\n")
+    return len(lines) - 1, len(lines[len(lines)-1])
+}
+
+func getLine(text string, line int) string {
+    lines := strings.Split(text, "\n")
+    if line < len(lines) {
+        return lines[line]
+    }
+    return ""
+}
+
+func getLineStartPos(text string, line int) int {
+    lines := strings.Split(text, "\n")
+    if line < len(lines) {
+        start := 0
+        for i := 0; i < line; i++ {
+            start += len(lines[i]) + 1 // +1 for newline character
+        }
+        return start
+    }
+    return 0
+}
+
+func getLineCount(text string) int {
+    return len(strings.Split(text, "\n"))
 }
